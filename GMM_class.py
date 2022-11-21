42998f8a924b1f0ac17f6d894a2a252b3088e021
saidamal <saedamsd@gmail.com>
2022-11-21 10:31:45 -0500

    updating gmm test and class and adding main_gmm

--- GMM_class.py
+++ GMM_class.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import numpy as np
-
+import os
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -9,39 +9,70 @@ from sklearn.model_selection import train_test_split
 from sklearn.mixture import GaussianMixture
 from sklearn.metrics import confusion_matrix
 
-class GMM_class_:
+
+class GMMClass:
     def __init__(
             self,
-            vocabulary,
+            path='storage/',
+            fileop="GMM_outputs.txt"
     ):
         self.n_classes = 0
         self.n_estimators = 0
-        self.colors = ["navy", "turquoise"]
-        self.estimators = None
-
-    #  Saeed - see how to get the code of GMM here since now we have the labels from get_labels
+        self.colors = []
+        self.estimators = {}
+        self.X_train = []
+        self.X_test = []
+        self.y_train = []
+        self.y_test = []
+        self.path = path
+        self.fileop = fileop
+        isExist = os.path.exists(self.path)
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs(self.path)
 
     def save_GMM(
             self,
-            path: str = '/storage/GMM_outputs.txt',
-            text: str = ''
+            text: str = '',
+            path: str = None,
+            fileop: str = None
     ):
-        with open(path, 'a') as f:
+        # self.path = path
+        if path != None:
+            Path = path
+        else:
+            Path = self.path
+        if fileop != None:
+            filePath = fileop
+        else:
+            filePath = self.fileop
+        with open(
+                self.path+filePath,
+                'a',
+        ) as f:
             f.write(text + '\n')
 
-    # Saeed - commented one here as np.unique(labels) is 3
-    # saeed opened again same error below - Error - IndexError: index 7 is out of bounds for axis 0 with size 7
-    # , "green"
-    #  ]
-
-    def split_train_test_data(self, topic_matrix, labels, ):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(topic_matrix, labels, test_size=0.25,
-                                                                                random_state=42)
-        # Saeed - removing the -1 samples points only from the train data and commenting the above y_arr!=-1
+    def split_train_test_data(
+            self,
+            topic_matrix,
+            labels,
+    ):
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+            train_test_split(
+                topic_matrix,
+                labels,
+                test_size=0.25,
+                random_state=42,
+            )
         self.X_train = self.X_train[self.y_train != -1]
         self.y_train = self.y_train[self.y_train != -1]
 
-    def make_ellipses(gmm, ax, colors):
+    def make_ellipses(
+            self,
+            gmm,
+            ax,
+            colors,
+    ):
         for n, color in enumerate(colors):
             if gmm.covariance_type == "full":
                 covariances = gmm.covariances_[n][:2, :2]
@@ -51,25 +82,41 @@ class GMM_class_:
                 covariances = np.diag(gmm.covariances_[n][:2])
             elif gmm.covariance_type == "spherical":
                 covariances = np.eye(gmm.means_.shape[1]) * gmm.covariances_[n]
+            else:
+                return
             v, w = np.linalg.eigh(covariances)
             u = w[0] / np.linalg.norm(w[0])
             angle = np.arctan2(u[1], u[0])
             angle = 180 * angle / np.pi  # convert to degrees
             v = 2.0 * np.sqrt(2.0) * np.sqrt(v)
             ell = mpl.patches.Ellipse(
-                gmm.means_[n, :2], v[0], v[1], 180 + angle, color=color
+                gmm.means_[n, :2],
+                v[0],
+                v[1],
+                180 + angle,
+                color=color,
             )
             ell.set_clip_box(ax.bbox)
             ell.set_alpha(0.5)
             ax.add_artist(ell)
-            ax.set_aspect("equal", "datalim")
+            ax.set_aspect("equal",
+                          "datalim",
+                          )
 
-    def GMM_init(self, colors, n_clusters, ):
-        # Try GMMs using different types of covariances.
+        return ax
+
+    def GMM_init(
+            self,
+            colors,
+            n_clusters,
+    ):
         estimators = {
-            cov_type: GaussianMixture(n_components=n_clusters, covariance_type=cov_type, max_iter=20, random_state=0)
-            # Saeed - here for showing only one to fix the accuracy issue
-            # for cov_type in ["spherical", "diag", "tied", "full"]
+            cov_type: GaussianMixture(
+                n_components=n_clusters,
+                covariance_type=cov_type,
+                max_iter=20,
+                random_state=0,
+            )
             for cov_type in ["spherical", "diag"]}
 
         self.n_estimators = len(estimators)
@@ -77,63 +124,53 @@ class GMM_class_:
         self.estimators = estimators
         return estimators
 
-    def GMM_validation_plot(self, X_train, y_train, estimator, name, index, h,):
-            h = plt.subplot(2, self.n_estimators // 2, index + 1)
-            self.make_ellipses(estimator, h, self.colors)
-            for n, color in enumerate(self.colors):
-                data = X_train[y_train == n]
-                plt.scatter(
-                    data[:, 0], data[:, 1], s=0.8, color=color, label=X_train[n]
-                )
-            y_train_pred = estimator.predict(X_train)
-            train_accuracy = np.mean(y_train_pred.ravel() == y_train.ravel()) * 100
-            plt.text(0.05, 0.9, "Train accuracy: %.1f" % train_accuracy, transform=h.transAxes)
 
-            plt.xticks(())
-            plt.yticks(())
-            plt.title(name)
-            self.save_GMM("Train accuracy: %.1f" % train_accuracy)
-            plt.savefig('/storage/GMM_plot_fig_train')
+    def GMM_train(
+            self,
+    ):
 
-    def GMM_train(self, X_train, y_train, colors, ):
-
-        self.n_classes = len(np.unique(y_train))
+        self.n_classes = len(np.unique(self.y_train))
 
         for index, (name, estimator) in enumerate(self.estimators.items()):
-            # Since we have class labels for the training data, we can
-            # initialize the GMM parameters in a supervised manner.
-
             estimator.means_init = np.array(
-                # Saeed - changed ot run over -1 to 5 not as here beecause of error below on the fit():
-                # [X_train[y_train == i].mean(axis=0) for i in range(-1,n_classes-1)]
-                [X_train[y_train == i].mean(axis=0) for i in range(0, self.n_classes)]
+                [self.X_train[self.y_train == i].mean(axis=0) for i in range(0, self.n_classes)]
             )
-            # Train the other parameters using the EM algorithm.
-            estimator.fit(X_train)
+            estimator.fit(self.X_train)
 
-            self.GMM_validation_plot(self, X_train, y_train, estimator, name)
-
-    def GMM_test_plot(self, X_test, y_test, colors, h, ):
+    def GMM_test_plot(
+            self,
+    ):
         n_estimators = len(self.estimators)
         for index, (name, estimator) in enumerate(self.estimators.items()):
-
             h = plt.subplot(2, n_estimators // 2, index + 1)
-            self.make_ellipses(estimator, h, colors)
-            # Plot the test data with crosses
-            for n, color in enumerate(colors):
-                data = X_test[y_test == n]
-                plt.scatter(data[:, 0], data[:, 1], marker="x", color=color)
+            self.make_ellipses(estimator, h, self.colors)
+            for n, color in enumerate(self.colors):
+                data = self.X_test[self.y_test == n]
+                plt.scatter(
+                    data[:, 0],
+                    data[:, 1],
+                    marker="x",
+                    color=color,
+                )
 
-            y_test_pred = estimator.predict(X_test)
-            test_accuracy = np.mean(y_test_pred.ravel() == y_test.ravel()) * 100
+            y_test_pred = estimator.predict(self.X_test)
+            test_accuracy = np.mean(y_test_pred.ravel() == self.y_test.ravel()) * 100
             self.save_GMM("Test accuracy: %.1f" % test_accuracy)
-            plt.text(0.05, 0.8, "Test accuracy: %.1f" % test_accuracy, transform=h.transAxes)
+            plt.text(
+                0.05,
+                0.8,
+                "Test accuracy: %.1f" % test_accuracy,
+                transform=h.transAxes,
+            )
 
-            cm = confusion_matrix(y_test, y_test_pred)
+            cm = confusion_matrix(
+                self.y_test,
+                y_test_pred,
+            )
             self.save_GMM("confusion matrix:\n" % cm)
-
             plt.xticks(())
             plt.yticks(())
             plt.title(name)
-            plt.savefig('/storage/GMM_plot_fig_test')
+            plt.savefig(self.path+'GMM_plot_fig_test')
+            return test_accuracy
 
--- test_GMM.py
+++ test_GMM.py
@@ -1,97 +1,201 @@
 import unittest
 import unittest.mock as mock
 
-from unittest.mock import patch
-import pandas as pd
+import matplotlib.axes
+import numpy as np
+import os
+import GMM_class
+from GMM_class import GMMClass
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
-from vocabolary_creator import create_vocab_class
 
-from GMM_class import GMM_class_
-
-
-class TestVocabClass(unittest.TestCase):
-
-    #For the class not the instance
-    def setUpClass(
-            cls,
-    ):
-
-        pass
-    def tearDownClass(
-            cls,
-    ):
-        pass
+class TestGMMClass(unittest.TestCase):
 
     def setUp(
-        self,
+            self,
     ) -> None:
         self.dataset_name = '20newsgroup'
+        self.X_train = []
+        self.y_train = []
+        self.X_test = []
+        self.y_test = []
+        self.topic_matrix = []
+        self.labels = []
+        self.colors = ["navy", "turquoise"]
 
-    def tearDown(
-            self,
-    ):
-        pass
-
-    def test_save_GMM(
-        self,
-        path: str = '/storage/GMM_outputs.txt',
-        text: str = ''
-
-    ):
-        pass
-
-    def test_split_train_test_data(self, topic_matrix, labels, ):
-        pass
-
-    def test_make_ellipses(self, gmm, ax, colors, ):
-        pass
-
-    def test_GMM_init(self, colors, n_clusters ):
-        with mock.patch(
-                target='sklearn.mixture.GaussianMixture',
-        ) as GaussianMixture_func:
-            GaussianMixture_func.return_value = None
-
-        gmm_instance = GMM_class_()
-        estimators = gmm_instance.GMM_init(self, ["navy", "turquoise"], 2)
-
-        self.assertEqual(
-            first=estimators,
-            second=None,
+        self.topic_matrix = np.array(
+            [[0.00166667, 0.00166667, 0.00166667, 0.14958999, 0.00166667, 0.00166667],
+             [0.00166667, 0.00166667, 0.00166667, 0.14958999, 0.00166667, 0.00166667],
+             [0.00166667, 0.00166667, 0.00166667, 0.14958999, 0.00166667, 0.00166667],
+             [0.00166667, 0.00166667, 0.00166667, 0.14958999, 0.00166667, 0.00166667], ]
         )
 
-    def test_GMM_validation_plot(self, X_train, y_train, estimator, name, index, h,):
+        self.labels = np.array([1, 0, 1, 0])
+        try:
+            os.remove("storage/GMM_outputs_test.txt")
+        except FileNotFoundError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("storage/GMM_outputs_test.txt")
+        except FileNotFoundError:
+            pass
+
+    def test_save_GMM(
+            self,
+            path: str = 'storage/',
+            fileop: str = 'GMM_outputs_test.txt',
+    ):
+        gmm_instance = GMMClass(path=path,fileop=fileop)
+        gmm_instance.save_GMM(
+            'text for printing',
+            path,
+            fileop,
+        )
+        isFile = os.path.isfile(path+fileop)
+        file = open(path+fileop, 'r')
+        self.assertEquals(isFile, True)
+        self.assertEquals(file.read(), 'text for printing\n')
+        file.close()
+        # if my_file.is_file():
+        # assert file exists
+        # assert if file contains values
+        # delete file
+
+    def test_split_train_test_data(
+            self,
+    ):
+        gmm_instance = GMMClass()
+        gmm_instance.split_train_test_data(
+            self.topic_matrix,
+            self.labels,
+        )
+
+        self.assertIsNotNone(gmm_instance.X_train)
+        self.assertIsNotNone(gmm_instance.y_train)
+        self.assertIsNotNone(gmm_instance.X_test)
+        self.assertIsNotNone(gmm_instance.y_test)
+
+        self.assertIs(type(gmm_instance.X_train), np.ndarray)
+        self.assertIs(type(gmm_instance.y_train), np.ndarray)
+        self.assertIs(type(gmm_instance.X_test), np.ndarray)
+        self.assertIs(type(gmm_instance.y_test), np.ndarray)
+        x_len = len(self.topic_matrix)
+        y_len = len(self.labels)
+        x_test_len = len(gmm_instance.X_test)
+        y_test_len = len(gmm_instance.y_test)
+        self.assertEquals(x_test_len / x_len, 0.25)
+        self.assertEquals(y_test_len / y_len, 0.25)
+
+        # asset t_train not -1
+        # assert x_train not -1
+        # check ratio of x_train and y_train to test xy allow for ratio to be +/- 0.05 to be sure
+        # check the labels
+
+
+    def test_make_ellipses(
+            self,
+    ):
+        with mock.MagicMock(
+                target='sklearn.mixture.GaussianMixture', ) as gmm_magic_mock:
+            gmm_instance = GMMClass()
+            # gmm_magic_mock.covariance_type = "spherical"
+            h = plt.subplot(
+                2,
+                2 // 2,
+                1 + 1,
+            )
+
+            mpl_ax = gmm_instance.make_ellipses(
+                gmm_magic_mock,
+                h,
+                ["navy", "turquoise"],
+            )
+            self.assertIs(mpl_ax, None)
+            # self.assertIs(mpl_ax, matplotlib.axes.Axes)
+            # assert what it returns
+
+
+    def test_GMM_init(
+            self,
+    ):
+        gmm_instance = GMMClass()
+        gmm_instance.GMM_init(
+            ["navy", "turquoise"],
+            2,
+        )
+        self.assertEqual(["navy", "turquoise"], gmm_instance.colors)
+        self.assertNotEqual(None, gmm_instance.estimators)
+
+    def test_GMM_train(
+            self,
+    ):
+
         with mock.patch(
-                target='matplotlib.pyplot',
+                target='GMM_class.GMMClass', ) as GMMClass_mock:
+            GMMClass_mock.return_value.GMM_validation_plot.return_value = None
+            # why this mock is needed GMM_train does not use validation plot internatlly , so whats the point of this ?
+            gmm_instance = GMMClass()
+            try:
+                gmm_instance.GMM_train()
+                self.assertTrue(True)
+            except:
+                self.assertTrue(False)
+
+            # this does not return anything , for testing to be done ,
+            # a functon must either return a value , or manipulate its own value which can be checked
+
+    def test_GMM_test_plot(
+            self,
+    ):
+        with mock.patch(
+            target='GMM_class.plt',
         ) as pyplot_mock, mock.patch(
-                target='GMM_class.GMM_class_.make_ellipses',) as make_ellipses_mock \
-                , mock.patch(target='GMM_class.GMM_class_.save_GMM', ) as save_GMM_mock:
+                target='GMM_class.GMMClass', ) as GMMClass_mock, mock.patch(
+                target='GMM_class.np', ) as np_mock, mock.patch(
+                target='GMM_class.GMMClass.GMM_validation_plot.len', ) as len_mock, mock.patch(
+                # target='GMM_class.GaussianMixture.predict',spec=np_mock.array([1, 0, 1, 0]) ) as GaussianMixture_predict_mock,mock.patch(
+                target='GMM_class.GaussianMixture', ) as GaussianMixture_mock,mock.patch(
+                target='sklearn.metrics.confusion_matrix',return_value = 'Mocked This Silly' ) :
+            np_mock.side_effect =[1, 0, 1, 0]
+            np_mock.spec = [1, 0, 1, 0]
+            np_mock.return_value = [1, 0, 1, 0]
+        #     # pyplot_mock.return_value = None
+        #     confusion_matrix_mock=metrics.return_value
+        #     confusion_matrix_mock.\
+        #     metrics_mock.confusion_matrix = None
 
-            make_ellipses_mock.return_value = None
-# Eran - shall we mock pyplo ??? its wont fail and par of the flow
-            pyplot_mock.subplot.return_value = None
-            pyplot_mock.scatter.return_value = None
-            pyplot_mock.xticks.return_value = None
-            pyplot_mock.yticks.return_value = None
-            pyplot_mock.title.return_value = None
-            pyplot_mock.text.return_value = None
-            pyplot_mock.savefig.return_value = None
-            save_GMM_mock.return_value = None
+            # confusion_matrix_mock.confusion_matrix.return_value = []
+            GMMClass_mock.return_value.make_ellipses.return_value = None
+            GaussianMixture_mock.covariance_type = None
 
-            gmm_instance = GMM_class_()
-            estimators = gmm_instance.GMM_validation_plot(self, X_train, y_train, estimator, name, index, h,)
+        #     # GMMClass_mock.return_value.save_GMM_mock.return_value = None
+        #     GMMClass_mock.return_value.confusion_matrix_mock.return_value = None
+        #     GMMClass_mock.return_value.GMM_validation_plot.return_value = None
+        #     GaussianMixture_predict_mock.return_value =  np_mock.array([1, 0, 1, 0])
+        #     len_mock.return_value = 10
+        #     len_mock.return_value = 10
+            gmm_instance = GMMClass(path="storage/",fileop="GMM_outputs_test.txt")
+            gmm_instance.split_train_test_data(
+                self.topic_matrix,
+                self.labels,
+            )
+            GaussianMixture_mock.return_value.predict.return_value = gmm_instance.y_test
+            gmm_instance.GMM_init(
+                colors=['red'],
+                n_clusters=1)
 
-    def test_GMM_train(self, X_train, y_train, colors,):
-        with mock.patch(target='GMM_class.GMM_class_.GMM_validation_plot_mock', ) as GMM_validation_plot_mock:
-
-            GMM_validation_plot_mock.return_value = None
-
-            gmm_instance = GMM_class_()
-            estimators = gmm_instance.GMM_train(X_train, y_train, colors)
-
-    def test_GMM_test_plot(self, X_test, y_test, colors, h, ):
-        pass
-
+            accuracy = gmm_instance.GMM_test_plot()
+            path = "storage/GMM_plot_fig_test"
+            isFile = os.path.isfile(path)
+            # self.assertEquals(isFile, True)
+            #print(gmm_instance.path+"GMM_outputs.txt")
+            isGmmFile = os.path.isfile("storage/GMM_outputs_test.txt")
+            self.assertEquals(isGmmFile, True)
+            # /storage/GMM_plot_fig_test
+            # confusion matrix in the file Save_GMM
 
 
 if __name__ == '__main__':
